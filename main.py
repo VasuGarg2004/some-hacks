fec2ebb44abc5f9164cb209a9c87b548d0a9a7e7
Vasu Garg <88959104+VasuGarg2004@users.noreply.github.com>
2022-12-01 22:09:22 +0530

    Idea implemented, bugs to be fixed

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+import json
 import hashlib as hl
 m = hl.sha256()
 
-# m.update(b"21ME10093")
-# str = m.hexdigest()
+# Get the secret Ids to a list
+with open('secret-id.txt') as file1:
+    secretIds = [line.rstrip() for line in file1]
 
-# secretId = str[len(str)-6:]
+# Get possible dep codes to a list
+with open('dep.txt') as file2:
+    deps = [line.rstrip() for line in file2]
 
-with open('secret-id.txt') as file:
-    secretIds = [line.rstrip() for line in file]
+# define output dict
+validSecretIds = {}
 
+# run a loop over deps for rolls in given range
+for dep in deps:
+    for i in range(10001-10200, 20001-20200, 30001-30200):
+        # concatenate roll number and find last 6 digits of sha256 hash
+        m.update(b"21" + dep + i)
+        str = m.hexdigest()
+
+        sid = str[len(str) - 6:]
+
+        # Verify against given list to approve roll as well as ID and store them in dict
+        for secretId in secretIds:
+            if sid == secretId:
+                validSecretIds["21" + dep + i] = secretId
+
+# output obtained data
+with open("Valid-Secret-Ids.json", "w") as outfile:
+    json.dump(validSecretIds, outfile)
